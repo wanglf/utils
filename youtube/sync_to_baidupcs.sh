8695b5da5fd785cf74cc88cdcaf5401da911eb65
wanglf <wlfqware@gmail.com>
2015-09-13 08:56:46 +0800

    revised sync_to_baidupcs.sh

--- youtube/sync_to_baidupcs.sh
+++ youtube/sync_to_baidupcs.sh
@@ -6,7 +6,7 @@ find /opt/youtube/ -name "*.mp4" -exec mv {} /opt/baidupcs/ \;
 # upload to baidu pcs
 cd /opt/baidupcs/ && /usr/bin/python /opt/git/bypy.git/bypy.py syncup
 # remove files created more than 7 days
-find /opt/baidupcs/ -ctime +7 -exec rm -fr {} \; > /dev/null 2>&1
+find /opt/baidupcs/* -ctime +7 -exec rm -fr {} \; > /dev/null 2>&1
 echo "------------------------------------------------------------"
 echo "`date` - finish executing syncup_to_baidupcs.sh"
 echo "------------------------------------------------------------"
